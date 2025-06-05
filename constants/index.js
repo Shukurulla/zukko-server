4d212f67136e328ba3fa70031d086d0da1c36b6c
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-06-05 14:13:41 +0500

    change video apis

--- constants/index.js
+++ constants/index.js
@@ -1,14 +1,14 @@
 export const teacherVideos = [
   {
     title: "Mental arifmetika 1-dars (Abakus bilan tanishuv)",
-    link: "https://kepket.uz/media/video16.mp4",
+    link: "https://kepket.uz/media/zukko/videos/1.mp4",
     id: 1,
     description:
       "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
   },
   {
     title: "Mental arifmetika 2-dars ",
-    link: "https://kepket.uz/media/video16.mp4",
+    link: "https://kepket.uz/media/zukko/videos/2.mp4",
     id: 2,
 
     description:
@@ -16,57 +16,50 @@ export const teacherVideos = [
   },
   {
     title: "Mental arifmetika 3-dars ",
-    link: "https://kepket.uz/media/video16.mp4",
+    link: "https://kepket.uz/media/zukko/videos/3.mp4",
     id: 3,
     description:
       "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
   },
   {
     title: "Mental arifmetika 4-dars ",
-    link: "https://kepket.uz/media/video16.mp4",
+    link: "https://kepket.uz/media/zukko/videos/4.mp4",
     id: 4,
     description:
       "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
   },
   {
-    title: "Mental arifmetika 4-dars ",
-    link: "https://kepket.uz/media/video16.mp4",
+    title: "Mental arifmetika 5-dars ",
+    link: "https://kepket.uz/media/zukko/videos/5.mp4",
     id: 5,
     description:
       "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
   },
   {
-    title: "Mental arifmetika 4-dars ",
-    link: "https://kepket.uz/media/video16.mp4",
+    title: "Mental arifmetika 6-dars ",
+    link: "https://kepket.uz/media/zukko/videos/6.mp4",
     id: 6,
     description:
       "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
   },
   {
-    title: "Mental arifmetika 4-dars ",
-    link: "https://kepket.uz/media/video16.mp4",
+    title: "Mental arifmetika 7-dars ",
+    link: "https://kepket.uz/media/zukko/videos/7.mp4",
     id: 7,
     description:
       "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
   },
   {
-    title: "Mental arifmetika 4-dars ",
-    link: "https://kepket.uz/media/video16.mp4",
+    title: "Mental arifmetika 8-dars ",
+    link: "https://kepket.uz/media/zukko/videos/8.mp4",
     id: 8,
     description:
       "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
   },
   {
-    title: "Mental arifmetika 4-dars ",
-    link: "https://kepket.uz/media/video16.mp4",
+    title: "Mental arifmetika 9-dars ",
+    link: "https://kepket.uz/media/zukko/videos/9.mp4",
     id: 9,
-    description:
-      "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
-  },
-  {
-    title: "Mental arifmetika 4-dars ",
-    link: "https://kepket.uz/media/video16.mp4",
-    id: 10,
     description:
       "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
   },
