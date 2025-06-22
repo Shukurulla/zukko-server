0c364738a1d841e34d04f81b992d3d7b66e9cb97
Shukurulla <shukurullatursinbayev978@gmai.com>
2025-06-22 14:38:32 +0500

    start

--- constants/index.js
+++ constants/index.js
@@ -1,67 +1,68 @@
 export const teacherVideos = [
   {
-    title: "Mental arifmetika 1-dars (Abakus bilan tanishuv)",
+    title: "Mantiqiy fikrlashga kirish",
     link: "https://kepket.uz/media/zukko/videos/1.mp4",
     id: 1,
     description:
-      "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
+      "Bu video orqali siz mantiqiy fikrlash nima ekanini, uning qanday turlari borligini va ayniqsa vizual-samarali fikrlashni qanday rivojlantirish mumkinligini o‘rganasiz. Darsda abakusdan foydalanish orqali fikrlashni o‘stirish yondashuvlari ko‘rsatiladi. Boshlang‘ich sinfda ishlovchi o‘qituvchilar uchun foydali metodik qo‘llanma sifatida tavsiya etiladi.",
   },
   {
-    title: "Mental arifmetika 2-dars ",
+    title: "Amaliy dars: Abakusda arifmetik amallar",
     link: "https://kepket.uz/media/zukko/videos/2.mp4",
     id: 2,
 
     description:
-      "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
+      "Ushbu videodarsda abakus – ya’ni hisob taxtachasi orqali qo‘shish va ayirish amallarini bajarish metodikasi yoritib beriladi. Darsda abakus yordamida boshlang‘ich sinf o‘quvchilarining amaliy fikrlash, qo‘l koordinatsiyasi, e’tibor va tafakkur tezligini rivojlantirish bo‘yicha yondashuvlar ko‘rsatiladi. Qo‘shish va ayirish jarayoni vizual-samarali tafakkur bilan bog‘liq holda tushuntiriladi va mantiqiy fikrlashni shakllantirishda samarali vosita sifatida talqin qilinadi. Ushbu material bo‘lajak boshlang‘ich sinf o‘qituvchilari uchun amaliy-metodik qo‘llanma vazifasini bajaradi.",
   },
   {
-    title: "Mental arifmetika 3-dars ",
+    title: "Har bir son – har xil obraz: bolalar uchun esda qolarli yondashuv",
     link: "https://kepket.uz/media/zukko/videos/3.mp4",
     id: 3,
     description:
-      "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
+      "Ushbu videodarsda boshlang‘ich sinf o‘quvchilarining vizual-majoziy tafakkuri asosida son tushunchalarini shakllantirish usullari ko‘rib chiqiladi. Darsning asosiy yondashuvi — obrazli-assotsiativ metod orqali sonlarni real ob’ektlar (masalan, qalam, dinozavr, kapalak, stul, yulduz, qulf, bumerang, qor bobo va h.k.) bilan bog‘lash orqali mustahkamlashga qaratilgan. \n Video davomida har bir son uchun bir nechta tanish obrazlar taklif etiladi, bu orqali bolalarning eslab qolish qobiliyati, qiyoslash, assotsiatsiya yaratish va mantiqiy fikrlashni rivojlantirish ko‘nikmalari shakllantiriladi. \n Ushbu material bo‘lajak boshlang‘ich sinf o‘qituvchilari uchun matematik tushunchalarni o‘zlashtirishda samarali metodik vosita sifatida xizmat qiladi.",
   },
   {
-    title: "Mental arifmetika 4-dars ",
+    title: "Sonlarni obrazlar bilan bog‘lash: o‘yinli yondashuvlar",
     link: "https://kepket.uz/media/zukko/videos/4.mp4",
     id: 4,
     description:
-      "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
+      "Ushbu videodarsda obrazli-assotsiativ metod asosida tashkil etilgan didaktik o‘yinlar orqali son tushunchalarini mustahkamlash yondashuvlari ko‘rsatib beriladi. \n Darsda o‘quvchilarga turli vizual kartochkalar, musobaqa va tanlov elementlari orqali raqamlar va ularga mos obrazlarni tanlash topshiriqlari beriladi. Bu jarayon nafaqat bolalarning sonlarni eslab qolishida, balki mantiqiy fikrlash, bog‘liqlikni aniqlash, qiyoslash va ijodiy yondashuv ko‘nikmalarini rivojlantiradi. \n Shuningdek, tanlovli test usuli orqali ikki xonali sonlarni obrazlar bilan bog‘lash amaliyoti ham namoyish qilinadi. \n Bu metodlar boshlang‘ich sinf o‘quvchilari uchun bilimlarni samarali, qiziqarli va innovatsion shaklda o‘zlashtirish imkonini beradi.",
   },
   {
-    title: "Mental arifmetika 5-dars ",
+    title:
+      "Son va obraz asosida ertak tuzish: didaktik o‘yinlar va sinektik animatsiya",
     link: "https://kepket.uz/media/zukko/videos/5.mp4",
     id: 5,
     description:
-      "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
+      "Ushbu videoda boshlang‘ich sinf o‘quvchilarining mantiqiy va obrazli tafakkurini rivojlantirish, raqam–obraz aloqasini mustahkamlash va og‘zaki izchillik asosida ijodiy ertak tuzish metodikasi amaliy misollar bilan namoyish etiladi. \n  Sinektik yondashuv” asosidagi ushbu faoliyat bolalarga o‘rgatilgan obrazli raqamlarni voqeaga aylantirish orqali ketma-ketlikni eslab qolish, tanqidiy fikrlash, va matematik tushunchalarni hikoya tarzida ifodalash ko‘nikmalarini shakllantiradi. \n O‘yin shaklidagi mashg‘ulotda har bir guruh o‘ziga berilgan obrazlar asosida ertak yaratadi, o‘qituvchi esa bu ertakni adabiy jihatdan boyitib yozib oladi. Keyingi bosqichda esa shu asosda animatsion multfilm yaratiladi. \n Natijada, o‘quvchilarda nafaqat sonlar va obrazlar o‘rtasidagi bog‘liqlik mustahkamlanadi, balki ularda ijodiy tafakkur, jamoaviy hamkorlik, va samarali ifoda ko‘nikmalari ham rivojlanadi.",
   },
   {
-    title: "Mental arifmetika 6-dars ",
+    title: "Son va buyumlarni bogʻlab eslab qolish: innovatsion yondashuv",
     link: "https://kepket.uz/media/zukko/videos/6.mp4",
     id: 6,
     description:
-      "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
+      "Ushbu videoda boshlang‘ich sinf o‘quvchilarining xotirasini rivojlantirish, ro‘yxat va ketma-ketlikni eslab qolish qobiliyatini oshirishga qaratilgan obrazli-assotsiativ metod va Kaizen tamoyiliga asoslangan didaktik yondashuv bayon etiladi. O‘quvchilarga beriladigan oddiy ro‘yxat, vizual obrazlarga bog‘lanadi va ular hikoya yoki ertak tarzida ifoda etiladi. \n Bu metodikada asosiy bosqichlar quyidagilar: ro‘yxat asosida vizual assotsiatsiyalar yaratish, asosiy elementlarni tanlab, ularni obrazlar bilan bog‘lash, hikoya to‘qish orqali yodlash va Kaizen tamoyiliga ko‘ra asta-sekin murakkablikni oshirib borish. \n Ushbu metod orqali o‘quvchilarda mustaqil fikrlash, tizimli eslab qolish, og‘zaki nutqni boyitish va mantiqiy bog‘liqlikni shakllantirish ko‘nikmalari rivojlanadi",
   },
   {
-    title: "Mental arifmetika 7-dars ",
+    title: "Raqamlar ertagi: Mnemonik-sinektik multfilm darslari",
     link: "https://kepket.uz/media/zukko/videos/7.mp4",
     id: 7,
     description:
-      "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
+      "Ushbu video orqali boshlang‘ich sinf o‘quvchilari ko‘paytirish jadvalidagi misollarni oddiy yodlash emas, balki ertakli syujet, vizual obraz va sarguzashtli multfilm orqali chuqur tushunib olishadi. Bu uslub — mnemonik-sinektik integrativ metod deb ataladi. Unda har bir misol (masalan, 2×2, 2×3, 2×7, 2×9) raqamlarning ifodaviy obrazlari orqali jonlantirilgan, ular oqqush, yelkanli kema, shar, kapalak, sok kabi jonli elementlar bilan uyg‘unlashtiriladi. \n Bolalar har bir misolni hikoya orqali eslab qoladilar. Masalan, “2 × 3 = 6” misolida oqqushlar gilos uzib yeb, kapalak bilan do‘stlashadi. “2 × 7 = 14” esa boomerang, bolakay va sok bilan bog‘langan kulgili sahna orqali yodlanadi. Har bir sahna — bu kichik multfilm, har bir misol — bu ertak!",
   },
   {
-    title: "Mental arifmetika 8-dars ",
+    title: "Raqamlar ertagi: Mnemonik-sinektik multfilm darslari",
     link: "https://kepket.uz/media/zukko/videos/8.mp4",
     id: 8,
     description:
-      "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
+      "Ushbu video orqali boshlang‘ich sinf o‘quvchilari ko‘paytirish jadvalidagi misollarni oddiy yodlash emas, balki ertakli syujet, vizual obraz va sarguzashtli multfilm orqali chuqur tushunib olishadi. Bu uslub — mnemonik-sinektik integrativ metod deb ataladi. Unda har bir misol (masalan, 2×2, 2×3, 2×7, 2×9) raqamlarning ifodaviy obrazlari orqali jonlantirilgan, ular oqqush, yelkanli kema, shar, kapalak, sok kabi jonli elementlar bilan uyg‘unlashtiriladi. \n Bolalar har bir misolni hikoya orqali eslab qoladilar. Masalan, “2 × 3 = 6” misolida oqqushlar gilos uzib yeb, kapalak bilan do‘stlashadi. “2 × 7 = 14” esa boomerang, bolakay va sok bilan bog‘langan kulgili sahna orqali yodlanadi. Har bir sahna — bu kichik multfilm, har bir misol — bu ertak!",
   },
   {
-    title: "Mental arifmetika 9-dars ",
+    title: "Obrazli yodlash o‘yini: multfilm ssenariysi asosida",
     link: "https://kepket.uz/media/zukko/videos/9.mp4",
     id: 9,
     description:
-      "🏫IQROKIDS Intellektni Rivojlantirish Markazi 👨‍🏫Pedagoglar uchun Mental Arifmetika kurslari",
+      "Mazkur video boshlang‘ich sinf o‘quvchilari uchun ko‘paytirish amallarini mnemonik ertak va vizual-assotsiativ obrazlar yordamida oson eslab qolish imkonini beradi. Lipuchkali didaktik o‘yin asosida tashkil etilgan metodika o‘quvchilarning: \n - mantiqiy tafakkurini, \n - eslab qolish qobiliyatini \n - tasavvur va mustaqil fikrlash ko‘nikmalarini \n mustahkamlab boradi \n Video uchta asosiy bosqichdan iborat: \n 1. Guruhlarga bo‘linish va obrazli raqamlarni olish. \n 2. Multfilm yoki ertakni eslab hikoya qilish. \n 3. Raqamlar orasidagi bog‘liqlikni tushunib, to‘g‘ri ko‘paytmani topish \n Bu metodikaning eng muhim jihati shundaki, u faqat raqamlarni o‘rgatmaydi, balki har bir bolani o‘z bilimini topishga, hikoya qilishga va tahlil qilishga o‘rgatadi. Shu sababli video nafaqat o‘quv-uslubiy, balki tarbiyaviy ahamiyatga ham egadir.",
   },
 ];
 export const studentVideos = [
