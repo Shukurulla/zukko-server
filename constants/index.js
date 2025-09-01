f58dfdc8dc5399c0507ae29768ff20aa50f06190
ShukurullaHackaton1 <sayidabdullatursinbayev205@gmail.com>
2025-09-02 01:07:51 +0500

    start

--- constants/index.js
+++ constants/index.js
@@ -1,14 +1,14 @@
 export const teacherVideos = [
   {
     title: "Mantiqiy fikrlashga kirish",
-    link: "https://kepket.uz/media/video1.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video1.mp4",
     id: 1,
     description:
       "Bu video orqali siz mantiqiy fikrlash nima ekanini, uning qanday turlari borligini va ayniqsa vizual-samarali fikrlashni qanday rivojlantirish mumkinligini o‘rganasiz. Darsda abakusdan foydalanish orqali fikrlashni o‘stirish yondashuvlari ko‘rsatiladi. Boshlang‘ich sinfda ishlovchi o‘qituvchilar uchun foydali metodik qo‘llanma sifatida tavsiya etiladi.",
   },
   {
     title: "Amaliy dars: Abakusda arifmetik amallar",
-    link: "https://kepket.uz/media/video2.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video2.mp4",
     id: 2,
 
     description:
@@ -16,14 +16,14 @@ export const teacherVideos = [
   },
   {
     title: "Har bir son – har xil obraz: bolalar uchun esda qolarli yondashuv",
-    link: "https://kepket.uz/media/video3.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video3.mp4",
     id: 3,
     description:
       "Ushbu videodarsda boshlang‘ich sinf o‘quvchilarining vizual-majoziy tafakkuri asosida son tushunchalarini shakllantirish usullari ko‘rib chiqiladi. Darsning asosiy yondashuvi — obrazli-assotsiativ metod orqali sonlarni real ob’ektlar (masalan, qalam, dinozavr, kapalak, stul, yulduz, qulf, bumerang, qor bobo va h.k.) bilan bog‘lash orqali mustahkamlashga qaratilgan. \n \n Video davomida har bir son uchun bir nechta tanish obrazlar taklif etiladi, bu orqali bolalarning eslab qolish qobiliyati, qiyoslash, assotsiatsiya yaratish va mantiqiy fikrlashni rivojlantirish ko‘nikmalari shakllantiriladi. \n \n Ushbu material bo‘lajak boshlang‘ich sinf o‘qituvchilari uchun matematik tushunchalarni o‘zlashtirishda samarali metodik vosita sifatida xizmat qiladi.",
   },
   {
     title: "Sonlarni obrazlar bilan bog‘lash: o‘yinli yondashuvlar",
-    link: "https://kepket.uz/media/video4.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video4.mp4",
     id: 4,
     description:
       "Ushbu videodarsda obrazli-assotsiativ metod asosida tashkil etilgan didaktik o‘yinlar orqali son tushunchalarini mustahkamlash yondashuvlari ko‘rsatib beriladi. \n \n Darsda o‘quvchilarga turli vizual kartochkalar, musobaqa va tanlov elementlari orqali raqamlar va ularga mos obrazlarni tanlash topshiriqlari beriladi. Bu jarayon nafaqat bolalarning sonlarni eslab qolishida, balki mantiqiy fikrlash, bog‘liqlikni aniqlash, qiyoslash va ijodiy yondashuv ko‘nikmalarini rivojlantiradi. \n \n Shuningdek, tanlovli test usuli orqali ikki xonali sonlarni obrazlar bilan bog‘lash amaliyoti ham namoyish qilinadi. \n \n Bu metodlar boshlang‘ich sinf o‘quvchilari uchun bilimlarni samarali, qiziqarli va innovatsion shaklda o‘zlashtirish imkonini beradi.",
@@ -31,28 +31,28 @@ export const teacherVideos = [
   {
     title:
       "Son va obraz asosida ertak tuzish: didaktik o‘yinlar va sinektik animatsiya",
-    link: "https://kepket.uz/media/video5.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video5.mp4",
     id: 5,
     description:
       "Ushbu videoda boshlang‘ich sinf o‘quvchilarining mantiqiy va obrazli tafakkurini rivojlantirish, raqam–obraz aloqasini mustahkamlash va og‘zaki izchillik asosida ijodiy ertak tuzish metodikasi amaliy misollar bilan namoyish etiladi. \n \n “Sinektik yondashuv” asosidagi ushbu faoliyat bolalarga o‘rgatilgan obrazli raqamlarni voqeaga aylantirish orqali ketma-ketlikni eslab qolish, tanqidiy fikrlash, va matematik tushunchalarni hikoya tarzida ifodalash ko‘nikmalarini shakllantiradi. \n \n O‘yin shaklidagi mashg‘ulotda har bir guruh o‘ziga berilgan obrazlar asosida ertak yaratadi, o‘qituvchi esa bu ertakni adabiy jihatdan boyitib yozib oladi. Keyingi bosqichda esa shu asosda animatsion multfilm yaratiladi. \n \n Natijada, o‘quvchilarda nafaqat sonlar va obrazlar o‘rtasidagi bog‘liqlik mustahkamlanadi, balki ularda ijodiy tafakkur, jamoaviy hamkorlik, va samarali ifoda ko‘nikmalari ham rivojlanadi.",
   },
   {
     title: "Son va buyumlarni bogʻlab eslab qolish: innovatsion yondashuv",
-    link: "https://kepket.uz/media/video6.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video6.mp4",
     id: 6,
     description:
       "Ushbu videoda boshlang‘ich sinf o‘quvchilarining xotirasini rivojlantirish, ro‘yxat va ketma-ketlikni eslab qolish qobiliyatini oshirishga qaratilgan obrazli-assotsiativ metod va Kaizen tamoyiliga asoslangan didaktik yondashuv bayon etiladi. O‘quvchilarga beriladigan oddiy ro‘yxat, vizual obrazlarga bog‘lanadi va ular hikoya yoki ertak tarzida ifoda etiladi. \n \n Bu metodikada asosiy bosqichlar quyidagilar: ro‘yxat asosida vizual assotsiatsiyalar yaratish, asosiy elementlarni tanlab, ularni obrazlar bilan bog‘lash, hikoya to‘qish orqali yodlash va Kaizen tamoyiliga ko‘ra asta-sekin murakkablikni oshirib borish. \n \n Ushbu metod orqali o‘quvchilarda mustaqil fikrlash, tizimli eslab qolish, og‘zaki nutqni boyitish va mantiqiy bog‘liqlikni shakllantirish ko‘nikmalari rivojlanadi.",
   },
   {
     title: "Raqamlar ertagi: Mnemonik-sinektik multfilm darslari",
-    link: "https://kepket.uz/media/video7.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video7.mp4",
     id: 7,
     description:
       "Ushbu video orqali boshlang‘ich sinf o‘quvchilari ko‘paytirish jadvalidagi misollarni oddiy yodlash emas, balki ertakli syujet, vizual obraz va sarguzashtli multfilm orqali chuqur tushunib olishadi. Bu uslub — mnemonik-sinektik integrativ metod deb ataladi. Unda har bir misol (masalan, 2×2, 2×3, 2×7, 2×9) raqamlarning ifodaviy obrazlari orqali jonlantirilgan, ular oqqush, yelkanli kema, shar, kapalak, sok kabi jonli elementlar bilan uyg‘unlashtiriladi. \n \n Bolalar har bir misolni hikoya orqali eslab qoladilar. Masalan, “2 × 3 = 6” misolida oqqushlar gilos uzib yeb, kapalak bilan do‘stlashadi. “2 × 7 = 14” esa boomerang, bolakay va sok bilan bog‘langan kulgili sahna orqali yodlanadi. Har bir sahna — bu kichik multfilm, har bir misol — bu ertak!",
   },
   {
     title: "Obrazli yodlash o‘yini: multfilm ssenariysi asosida",
-    link: "https://kepket.uz/media/video8.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video8.mp4",
     id: 8,
     description:
       "Mazkur video boshlang‘ich sinf o‘quvchilari uchun ko‘paytirish amallarini mnemonik ertak va vizual-assotsiativ obrazlar yordamida oson eslab qolish imkonini beradi. Lipuchkali didaktik o‘yin asosida tashkil etilgan metodika o‘quvchilarning: \n \n \n - mantiqiy tafakkurini, \n \n - eslab qolish qobiliyatini, \n \n - tasavvur va mustaqil fikrlash ko‘nikmalarini \n \n mustahkamlab boradi. \n \n Video uchta asosiy bosqichdan iborat: \n \n 1. Guruhlarga bo‘linish va obrazli raqamlarni olish. \n \n 2. Multfilm yoki ertakni eslab hikoya qilish. \n \n 3. Raqamlar orasidagi bog‘liqlikni tushunib, to‘g‘ri ko‘paytmani topish. \n \n Bu metodikaning eng muhim jihati shundaki, u faqat raqamlarni o‘rgatmaydi, balki har bir bolani o‘z bilimini topishga, hikoya qilishga va tahlil qilishga o‘rgatadi. Shu sababli video nafaqat o‘quv-uslubiy, balki tarbiyaviy ahamiyatga ham egadir",
@@ -61,14 +61,14 @@ export const teacherVideos = [
 export const studentVideos = [
   {
     title: "Mantiqiy fikrlashga kirish",
-    link: "https://kepket.uz/media/video1.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video1.mp4",
     id: 1,
     description:
       "Bu video orqali siz mantiqiy fikrlash nima ekanini, uning qanday turlari borligini va ayniqsa vizual-samarali fikrlashni qanday rivojlantirish mumkinligini o‘rganasiz. Darsda abakusdan foydalanish orqali fikrlashni o‘stirish yondashuvlari ko‘rsatiladi. Boshlang‘ich sinfda ishlovchi o‘qituvchilar uchun foydali metodik qo‘llanma sifatida tavsiya etiladi.",
   },
   {
     title: "Amaliy dars: Abakusda arifmetik amallar",
-    link: "https://kepket.uz/media/video2.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video2.mp4",
     id: 2,
 
     description:
@@ -76,14 +76,14 @@ export const studentVideos = [
   },
   {
     title: "Har bir son – har xil obraz: bolalar uchun esda qolarli yondashuv",
-    link: "https://kepket.uz/media/video3.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video3.mp4",
     id: 3,
     description:
       "Ushbu videodarsda boshlang‘ich sinf o‘quvchilarining vizual-majoziy tafakkuri asosida son tushunchalarini shakllantirish usullari ko‘rib chiqiladi. Darsning asosiy yondashuvi — obrazli-assotsiativ metod orqali sonlarni real ob’ektlar (masalan, qalam, dinozavr, kapalak, stul, yulduz, qulf, bumerang, qor bobo va h.k.) bilan bog‘lash orqali mustahkamlashga qaratilgan. \n \n Video davomida har bir son uchun bir nechta tanish obrazlar taklif etiladi, bu orqali bolalarning eslab qolish qobiliyati, qiyoslash, assotsiatsiya yaratish va mantiqiy fikrlashni rivojlantirish ko‘nikmalari shakllantiriladi. \n \n Ushbu material bo‘lajak boshlang‘ich sinf o‘qituvchilari uchun matematik tushunchalarni o‘zlashtirishda samarali metodik vosita sifatida xizmat qiladi.",
   },
   {
     title: "Sonlarni obrazlar bilan bog‘lash: o‘yinli yondashuvlar",
-    link: "https://kepket.uz/media/video4.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video4.mp4",
     id: 4,
     description:
       "Ushbu videodarsda obrazli-assotsiativ metod asosida tashkil etilgan didaktik o‘yinlar orqali son tushunchalarini mustahkamlash yondashuvlari ko‘rsatib beriladi. \n \n Darsda o‘quvchilarga turli vizual kartochkalar, musobaqa va tanlov elementlari orqali raqamlar va ularga mos obrazlarni tanlash topshiriqlari beriladi. Bu jarayon nafaqat bolalarning sonlarni eslab qolishida, balki mantiqiy fikrlash, bog‘liqlikni aniqlash, qiyoslash va ijodiy yondashuv ko‘nikmalarini rivojlantiradi. \n \n Shuningdek, tanlovli test usuli orqali ikki xonali sonlarni obrazlar bilan bog‘lash amaliyoti ham namoyish qilinadi. \n \n Bu metodlar boshlang‘ich sinf o‘quvchilari uchun bilimlarni samarali, qiziqarli va innovatsion shaklda o‘zlashtirish imkonini beradi.",
@@ -91,28 +91,28 @@ export const studentVideos = [
   {
     title:
       "Son va obraz asosida ertak tuzish: didaktik o‘yinlar va sinektik animatsiya",
-    link: "https://kepket.uz/media/video5.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video5.mp4",
     id: 5,
     description:
       "Ushbu videoda boshlang‘ich sinf o‘quvchilarining mantiqiy va obrazli tafakkurini rivojlantirish, raqam–obraz aloqasini mustahkamlash va og‘zaki izchillik asosida ijodiy ertak tuzish metodikasi amaliy misollar bilan namoyish etiladi. \n \n “Sinektik yondashuv” asosidagi ushbu faoliyat bolalarga o‘rgatilgan obrazli raqamlarni voqeaga aylantirish orqali ketma-ketlikni eslab qolish, tanqidiy fikrlash, va matematik tushunchalarni hikoya tarzida ifodalash ko‘nikmalarini shakllantiradi. \n \n O‘yin shaklidagi mashg‘ulotda har bir guruh o‘ziga berilgan obrazlar asosida ertak yaratadi, o‘qituvchi esa bu ertakni adabiy jihatdan boyitib yozib oladi. Keyingi bosqichda esa shu asosda animatsion multfilm yaratiladi. \n \n Natijada, o‘quvchilarda nafaqat sonlar va obrazlar o‘rtasidagi bog‘liqlik mustahkamlanadi, balki ularda ijodiy tafakkur, jamoaviy hamkorlik, va samarali ifoda ko‘nikmalari ham rivojlanadi.",
   },
   {
     title: "Son va buyumlarni bogʻlab eslab qolish: innovatsion yondashuv",
-    link: "https://kepket.uz/media/video6.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video6.mp4",
     id: 6,
     description:
       "Ushbu videoda boshlang‘ich sinf o‘quvchilarining xotirasini rivojlantirish, ro‘yxat va ketma-ketlikni eslab qolish qobiliyatini oshirishga qaratilgan obrazli-assotsiativ metod va Kaizen tamoyiliga asoslangan didaktik yondashuv bayon etiladi. O‘quvchilarga beriladigan oddiy ro‘yxat, vizual obrazlarga bog‘lanadi va ular hikoya yoki ertak tarzida ifoda etiladi. \n \n Bu metodikada asosiy bosqichlar quyidagilar: ro‘yxat asosida vizual assotsiatsiyalar yaratish, asosiy elementlarni tanlab, ularni obrazlar bilan bog‘lash, hikoya to‘qish orqali yodlash va Kaizen tamoyiliga ko‘ra asta-sekin murakkablikni oshirib borish. \n \n Ushbu metod orqali o‘quvchilarda mustaqil fikrlash, tizimli eslab qolish, og‘zaki nutqni boyitish va mantiqiy bog‘liqlikni shakllantirish ko‘nikmalari rivojlanadi.",
   },
   {
     title: "Raqamlar ertagi: Mnemonik-sinektik multfilm darslari",
-    link: "https://kepket.uz/media/video7.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video7.mp4",
     id: 7,
     description:
       "Ushbu video orqali boshlang‘ich sinf o‘quvchilari ko‘paytirish jadvalidagi misollarni oddiy yodlash emas, balki ertakli syujet, vizual obraz va sarguzashtli multfilm orqali chuqur tushunib olishadi. Bu uslub — mnemonik-sinektik integrativ metod deb ataladi. Unda har bir misol (masalan, 2×2, 2×3, 2×7, 2×9) raqamlarning ifodaviy obrazlari orqali jonlantirilgan, ular oqqush, yelkanli kema, shar, kapalak, sok kabi jonli elementlar bilan uyg‘unlashtiriladi. \n \n Bolalar har bir misolni hikoya orqali eslab qoladilar. Masalan, “2 × 3 = 6” misolida oqqushlar gilos uzib yeb, kapalak bilan do‘stlashadi. “2 × 7 = 14” esa boomerang, bolakay va sok bilan bog‘langan kulgili sahna orqali yodlanadi. Har bir sahna — bu kichik multfilm, har bir misol — bu ertak!",
   },
   {
     title: "Obrazli yodlash o‘yini: multfilm ssenariysi asosida",
-    link: "https://kepket.uz/media/video8.mp4",
+    link: "https://vpsserver.kerek.uz/videos/video8.mp4",
     id: 8,
     description:
       "Mazkur video boshlang‘ich sinf o‘quvchilari uchun ko‘paytirish amallarini mnemonik ertak va vizual-assotsiativ obrazlar yordamida oson eslab qolish imkonini beradi. Lipuchkali didaktik o‘yin asosida tashkil etilgan metodika o‘quvchilarning: \n \n \n - mantiqiy tafakkurini, \n \n - eslab qolish qobiliyatini, \n \n - tasavvur va mustaqil fikrlash ko‘nikmalarini \n \n mustahkamlab boradi. \n \n Video uchta asosiy bosqichdan iborat: \n \n 1. Guruhlarga bo‘linish va obrazli raqamlarni olish. \n \n 2. Multfilm yoki ertakni eslab hikoya qilish. \n \n 3. Raqamlar orasidagi bog‘liqlikni tushunib, to‘g‘ri ko‘paytmani topish. \n \n Bu metodikaning eng muhim jihati shundaki, u faqat raqamlarni o‘rgatmaydi, balki har bir bolani o‘z bilimini topishga, hikoya qilishga va tahlil qilishga o‘rgatadi. Shu sababli video nafaqat o‘quv-uslubiy, balki tarbiyaviy ahamiyatga ham egadir",
